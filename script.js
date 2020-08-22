59ad7e1f8f010264550329baa1951ca222bdba17
njeruisaac <njeruisaac@gmail.com>
2020-08-22 08:47:52 +0300

    add year variable

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 function results() {
     var month =parseInt(document.getElementById("month").value);
+    var year =parseInt(document.getElementById("year").value);

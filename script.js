d3de4947b9fc874cc4465ee67ee2fe37d96ac510
njeruisaac <njeruisaac@gmail.com>
2020-08-22 08:46:35 +0300

    add function results on month variable

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+function results() {
+    var month =parseInt(document.getElementById("month").value);
